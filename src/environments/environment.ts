183ba83d4d739c95c53b9c0dd9ee218f955e2ace
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-07-20 22:44:15 +0200

    lecfour

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+//  baseApi:"http://localhost:3000/",
 
 export const environment = {
+  baseApi:"https://fakestoreapi.com/",
   production: false
 };
 
